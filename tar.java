6147862bfc13163b7dab6e139ce0cc53d206ef57
Nandy21 <nandhinimuthulakshmi@gmail.com>
2016-09-08 17:21:28 +0530

    Create tar.java

---
+++ tar.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+
+public class tar {
+	
+	public static void main(String[] args) {
+		Scanner sc=new Scanner (System.in);
+		int[] a=new int[4];
+		int count=0;
+		for(int i=0;i<4;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		int t=sc.nextInt();
+		for(int i=0;i<4;i++)
+		{
+			for(int j=0;j<4;j++)
+			{
+				if(a[i]+a[j]==t)
+				{
+					count++;
+					if(count==1)
+						System.out.println(a[i]+" "+a[j]);
+					
+				}
+			}
+		}
+		
+	}
+
+}
